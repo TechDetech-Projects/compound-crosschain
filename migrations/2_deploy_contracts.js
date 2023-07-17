46a835d11d786b8d89b097ed3f75b22cbce62c12
Efrainq07 <efrainq07@hotmail.com>
2023-07-17 15:40:55 -0500

    feat: migrations for deployment of compound contracts into ganache local testnet

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,15 @@
 var Comptroller = artifacts.require("Comptroller");
+var CEther = artifacts.require("CEther");
+var InterestRateModel = artifacts.require("WhitePaperInterestRateModel");
 
 module.exports = function(deployer) {
   deployer.deploy(Comptroller);
+  deployer.deploy(InterestRateModel,
+    )
+  deployer.deploy(CEther,
+    Comptroller.address,
+    InterestRateModel.address,
+    
+    )
+
 };
